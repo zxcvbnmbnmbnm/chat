8afdffc50490aaade6c010718802031bcd06d76e
Bharath-designer <bharathv.developer@gmail.com>
2023-02-17 18:53:25 +0530

    responsive

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
   const [myId, setMyId] = useState();
   const [sendId, setSendId] = useState();
-
+  const [auth, SetAuth] = useState(false);
   const [chat, setChat] = useState([]);
 
   const connect = () => {
@@ -20,6 +20,7 @@ const App = () => {
       console.log(msg);
       setChat((prev) => [...prev, { class: "sender", data: msg.message }]);
     });
+    SetAuth(true)
   };
 
   const disconnect = () => {
@@ -38,35 +39,50 @@ const App = () => {
     });
   };
 
+  useEffect(() => {
+    return () => {
+      if (socket) socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="body">
-      <div className="inp">
-        <input
-          placeholder="My ID"
-          onChange={(e) => setMyId(e.target.value)}
-          type="text"
-        />
-        <button onClick={connect}>Connect</button>
-        <button onClick={disconnect}>Disconnect</button>
-      </div>
-      <div className="inp">
-        <input
-          placeholder="Send To"
-          onChange={(e) => setSendId(e.target.value)}
-          type="text"
-        />
-        <input placeholder="Type a message" ref={inpRef} type="text" />
-        <button onClick={handleSend}>Send</button>
-      </div>
-      <div className="chat">
-        {chat.map((obj, ind) => {
-          return (
-            <div key={ind} className={`${obj.class}`}>
-              {obj.data}
-            </div>
-          );
-        })}
-      </div>
+      {auth || (
+        <div className="inp">
+          <div className="getid">
+            <input
+              placeholder="My ID"
+              onChange={(e) => setMyId(e.target.value)}
+              type="text"
+            />
+            <button onClick={connect}>Connect</button>
+          </div>
+        </div>
+      )}
+      {auth && (
+        <div className="flexxx">
+          <div className="inp nott">
+            <input
+              placeholder="Send To"
+              onChange={(e) => setSendId(e.target.value)}
+              type="text"
+            />
+          </div>
+          <div className="chat">
+            {chat.map((obj, ind) => {
+              return (
+                <div key={ind} className={`${obj.class}`}>
+                  {obj.data}
+                </div>
+              );
+            })}
+          </div>
+          <div className="inp nott lastt">
+            <input placeholder="Type a message" ref={inpRef} type="text" />
+            <button onClick={handleSend}>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
